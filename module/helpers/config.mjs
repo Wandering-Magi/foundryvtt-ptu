8bf13a080b5ea7bf35445beabc23fdcbd750d388
Wandering-Magi <Wandering_Magi@hotmail.com>
2023-01-12 00:30:14 -0700

    Initial file dump

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const PTU = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ PTU.abilities = {
+  "str": "PTU.AbilityStr",
+  "dex": "PTU.AbilityDex",
+  "con": "PTU.AbilityCon",
+  "int": "PTU.AbilityInt",
+  "wis": "PTU.AbilityWis",
+  "cha": "PTU.AbilityCha"
+};
+
+PTU.abilityAbbreviations = {
+  "str": "PTU.AbilityStrAbbr",
+  "dex": "PTU.AbilityDexAbbr",
+  "con": "PTU.AbilityConAbbr",
+  "int": "PTU.AbilityIntAbbr",
+  "wis": "PTU.AbilityWisAbbr",
+  "cha": "PTU.AbilityChaAbbr"
+};
